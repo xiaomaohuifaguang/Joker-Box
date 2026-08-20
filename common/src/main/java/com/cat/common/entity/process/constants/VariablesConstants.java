0a542221f60aad5d75b6c68d6b085d44d9a0da86
s1x6 <xiaomaohuifaguang@163.com>
2026-08-21 00:39:58 +0800

    :poop:

---
+++ common/src/main/java/com/cat/common/entity/process/constants/VariablesConstants.java
@@ -0,0 +1,11 @@
+package com.cat.common.entity.process.constants;
+
+public class VariablesConstants {
+
+    public static final String CHOOSE_PRE = "choose_";
+
+
+    public static final String CACHE_ASSIGNEES = "cachedAssignees_";
+
+
+}
